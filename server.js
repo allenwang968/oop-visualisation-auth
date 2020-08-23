eb954c0f1bf8d89e7c9f952e4b426e7598aaa4d5
unknown <awan968@aucklanduni.ac.nz>
2020-08-23 23:49:48 +1200

    code

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
         // } else {
         //     res.render('survey.ejs')
         // }  
-        res.redirect('https://webgl-build-oop-visualisation.herokuapp.com/')
+        res.redirect('https://oop-visualisation-webgl.herokuapp.com/')
     }
 })
 
